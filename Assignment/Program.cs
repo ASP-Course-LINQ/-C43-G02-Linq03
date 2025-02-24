23e8e8c41fb8c6be42134c65940a85ebe9c3d815
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-02-24 23:26:36 +0200

    Part02 - Grouping Operators

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,14 @@
-﻿using static Assignment.ListGenerator;
+﻿using System.Collections.Generic;
+using System.Runtime.Intrinsics.X86;
+using static Assignment.ListGenerator;
+using static System.Net.Mime.MediaTypeNames;
 namespace Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
             #region Part01 - Partitioning Operators
 
             #region P01|Q01 - Get the first 3 orders from customers in Washington
@@ -48,6 +52,62 @@ namespace Assignment
             #endregion
 
             #endregion
+
+            #region Part02 - Grouping Operators
+
+            #region P02|Q01 - Use group by to partition a list of numbers by their remainder when divided by 5
+
+            //List<int> numbers = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+            //var result = numbers.GroupBy(num => num % 5);
+
+            //foreach (var remainder in result)
+            //{
+            //    Console.WriteLine($"Numbers With remainder of {remainder.Key} when divided by 5: ");
+            //    foreach (var num in remainder)
+            //    {
+            //        Console.WriteLine(num);
+            //    }
+            //}
+
+            #endregion
+
+            #region P02|Q02 - Uses group by to partition a list of words by their first letter. Use dictionary_english.txt for Input
+
+            //string[] txtFile = File.ReadAllLines("dictionary_english.txt");
+
+            //var result = txtFile.GroupBy(t => t.ElementAt(0));
+
+            //foreach (var firstChar in result)
+            //{
+            //    Console.WriteLine($"\nWords That Has char '{firstChar.Key}' As First Char:");
+            //    foreach (var word in firstChar)
+            //    {
+            //        Console.WriteLine($"  {word}");
+            //    }
+            //}
+
+            #endregion
+
+            #region P02|Q03 - Use Group By with a custom comparer that matches words that are consists of the same Characters Together
+
+            //string[] Arr = { "from", "salt", "earn", "last", "near", "form" };
+
+            //var result = Arr.GroupBy(w => w, new StringComparer());
+
+            //foreach (var item01 in result)
+            //{
+            //    foreach (var item02 in item01)
+            //    {
+            //        Console.WriteLine(item02);
+            //    }
+            //    Console.WriteLine("....");
+            //}
+
+            #endregion
+
+            #endregion
+
         }
     }
 }
---
+++ Assignment/StringComparer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class StringComparer : IEqualityComparer<string>
+    {
+        public bool Equals(string x, string y)
+        {
+            return new string(x.OrderBy(c => c).ToArray()) == new string(y.OrderBy(c => c).ToArray());
+        }
+
+        public int GetHashCode(string obj)
+        {
+            return new string(obj.OrderBy(c => c).ToArray()).GetHashCode();
+        }
+
+    }
+}
